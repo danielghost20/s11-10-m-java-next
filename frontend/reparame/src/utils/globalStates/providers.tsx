893d65d9cd163ad1d52ead76a1ab9cf4db1d9b4b
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2023-10-17 13:16:42 -0300

    joaquin: working on dynamic header by params

--- frontend/reparame/src/utils/globalStates/providers.tsx
+++ frontend/reparame/src/utils/globalStates/providers.tsx
@@ -2,14 +2,18 @@
 import { Provider } from 'react-redux'
 import { store } from './store'
 import Header from '@/components/header/Header'
+import { useParams } from 'next/navigation'
+
+
 
 function Providers({ children,
 }: {
     children: React.ReactNode
 }) {
+    const rute = useParams()
     return (
         <Provider store={store}>
-            <Header />
+            {rute.servicios === 'servicios' ? <Header /> : <></>}
             <main className='flex items-center justify-center'>
                 {children}
             </main>
