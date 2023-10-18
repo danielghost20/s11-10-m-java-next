a60c2f53c8aed9047602371e8c3b3ba5aabe186d
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2023-10-18 19:33:28 -0300

    header dinamico

--- frontend/reparame/src/components/home/Home.tsx
+++ frontend/reparame/src/components/home/Home.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import IconServicios from './cardServicios/IconServicios'
+import { useAppDispatch } from '@/utils/globalStates/hooks'
+import { setLog } from '@/utils/globalStates/features/pathSlice'
 
 const HomePage = () => {
+    const dispatch = useAppDispatch()
+    useEffect(() => {
+        dispatch(setLog('home'))
+    }, [dispatch])
     return (
         <section className='max-w-max-view w-full'>
             <h1>Home</h1>
